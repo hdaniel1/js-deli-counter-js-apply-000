0d114a5075241b14c8e3093e359b006b0209acf3
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-20 17:51:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ function currentLine(line) {
     return 'The line is currently empty.'
   }
   var array = []
-  var i = 0
-  while (line[i]) {
+  for (let i = 0; i < line.length; i++) {
     array.push(` ${i+1}. ${line[i]}`)
-    i++
   }
   return `The line is currently:${array.toString()}`
 }
